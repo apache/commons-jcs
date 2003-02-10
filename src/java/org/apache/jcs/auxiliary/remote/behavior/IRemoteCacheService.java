ff742e5f8cae8e70f4d11b5f21f8a20742e30ae1
James Taylor <jtaylor@apache.org>
2003-02-10 20:51:19 +0000

    Style fix (my fault, not the patch contributor)
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jcs/trunk@224034 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/jcs/auxiliary/remote/behavior/IRemoteCacheService.java
+++ src/java/org/apache/jcs/auxiliary/remote/behavior/IRemoteCacheService.java
@@ -34,5 +34,6 @@ public interface IRemoteCacheService extends Remote, ICacheService
     public void removeAll( String cacheName, byte requesterId )
         throws IOException;
 
-    public Set getGroupKeys(String cacheName, String groupName) throws IOException;
+    public Set getGroupKeys(String cacheName, String groupName) 
+        throws IOException;
 }
