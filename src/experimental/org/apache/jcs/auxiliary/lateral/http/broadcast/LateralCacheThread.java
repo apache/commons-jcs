88f7f32189c9109f62d4f536590b791cbdba188a
Aaron Smuts <asmuts@apache.org>
2004-04-15 19:22:59 +0000

    Updating license to 2.0.
    Used Jeffery Painter's script.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jcs/trunk@224069 13f79535-47bb-0310-9956-ffa450edef68

--- src/experimental/org/apache/jcs/auxiliary/lateral/http/broadcast/LateralCacheThread.java
+++ src/experimental/org/apache/jcs/auxiliary/lateral/http/broadcast/LateralCacheThread.java
@@ -1,5 +1,21 @@
 package org.apache.jcs.auxiliary.lateral.http.broadcast;
 
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -18,11 +34,6 @@ import org.apache.jcs.engine.behavior.ICacheElement;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-/**
- * @author Aaron Smuts
- * @created January 15, 2002
- * @version 1.0
- */
 public class LateralCacheThread extends Thread
 {
     private final static Log log =
