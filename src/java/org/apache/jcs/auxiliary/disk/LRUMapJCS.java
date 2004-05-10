7402520db61b29ad0838d1036cb58922bf75cbf5
Aaron Smuts <asmuts@apache.org>
2004-05-10 01:15:26 +0000

    New working prototype of disk key managment.  I don't like the startup size overhead problems with the commons LRUMap.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jcs/trunk@224081 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/jcs/auxiliary/disk/LRUMapJCS.java
@@ -0,0 +1,36 @@
+package org.apache.jcs.auxiliary.disk;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.collections.LRUMap;
+
+/**
+  *  Extension of LRUMap for logging of removals.
+  *  Can switch this back to a HashMap easily.
+  */
+public class LRUMapJCS extends LRUMap
+{
+
+  private static final Log log =
+        LogFactory.getLog( LRUMapJCS.class );
+
+ public LRUMapJCS()
+ {
+   super();
+ }
+
+ public LRUMapJCS( int maxKeySize )
+ {
+   super( maxKeySize );
+ }
+
+ protected void processRemovedLRU( Object key, Object value )
+ {
+   if ( log.isDebugEnabled() )
+   {
+     log.debug( "Removing key: '" + key + "' from key store." );
+     log.debug( "Key store size: '" + this.size() + "'." );
+   }
+
+ }
+}
