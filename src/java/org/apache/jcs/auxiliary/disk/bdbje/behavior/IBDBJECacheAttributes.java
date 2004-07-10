790df314592e6203481fb21e2c3e7ad4b0238453
Aaron Smuts <asmuts@apache.org>
2004-07-10 03:47:16 +0000

    Initial commit of an interface for the BDBJE attributes
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jcs/trunk@224116 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/jcs/auxiliary/disk/bdbje/behavior/IBDBJECacheAttributes.java
@@ -0,0 +1,43 @@
+package org.apache.jcs.auxiliary.disk.bdbje.behavior;
+
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import org.apache.jcs.auxiliary.AuxiliaryCacheAttributes;
+
+public interface IBDBJECacheAttributes
+{
+  public String getCacheName();
+
+  public int getCachePercent();
+
+  public long getCacheSize();
+
+  public String getDiskPath();
+
+  public String getName();
+
+  public void setCacheName(String s);
+
+  public void setCachePercent(int perc);
+
+  public void setCacheSize(long size);
+
+  public void setDiskPath(String path);
+
+  public void setName(String s);
+}
