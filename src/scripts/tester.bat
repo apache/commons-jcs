03143c095316f96775a09fd894863f7776c2bf46
Aaron Smuts <asmuts@apache.org>
2004-07-13 02:14:57 +0000

    Added -verbosegc for watching the garbage collector.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jcs/trunk@224126 13f79535-47bb-0310-9956-ffa450edef68

--- src/scripts/tester.bat
+++ src/scripts/tester.bat
@@ -5,7 +5,7 @@ call prep.bat
 rem-Dlog4j.configuration=I:/dev/jakarta-turbine-jcs/src/scripts/log4j.properties
    
 :run
-java -ms10m -mx400m  org.apache.jcs.access.TestCacheAccess /cache%1.ccf  %2 %3 %4 %5
+java -ms90m -mx400m -verbosegc org.apache.jcs.access.TestCacheAccess /cache%1.ccf  %2 %3 %4 %5
  
 
 
