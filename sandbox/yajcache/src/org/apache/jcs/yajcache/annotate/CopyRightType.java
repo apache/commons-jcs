5c356c65286c4ac4aeaeb941523915bcb6e2031a
Hanson Char <hchar@apache.org>
2005-01-22 21:41:26 +0000

    fix year
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jcs/trunk@224215 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/yajcache/src/org/apache/jcs/yajcache/annotate/CopyRightType.java
+++ sandbox/yajcache/src/org/apache/jcs/yajcache/annotate/CopyRightType.java
@@ -27,7 +27,7 @@ public enum CopyRightType {
         @Override public String toString() {
             return "\n"
             + "/* ========================================================================\n" 
-            + " * Copyright 2004 The Apache Software Foundation\n"
+            + " * Copyright 2001-2004 The Apache Software Foundation\n"
             + " *\n"
             + " * Licensed under the Apache License, Version 2.0 (the \"License\");\n"
             + " * you may not use this file except in compliance with the License.\n"
