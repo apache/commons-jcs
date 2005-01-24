b978fcf179bf34856cea97799fb38bdd857c7d99
Hanson Char <hchar@apache.org>
2005-01-24 10:30:39 +0000

    junit tests for cache file system
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jcs/trunk@224221 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sandbox/yajcache/test/org/apache/jcs/yajcache/file/FileContentTypeTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jcs.yajcache.file;
+
+import junit.framework.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.apache.jcs.yajcache.annotate.*;
+
+/**
+ *
+ * @author Hanson Char
+ */
+@CopyRightApache
+public class FileContentTypeTest extends TestCase {
+    private Log log = LogFactory.getLog(this.getClass());
+    /**
+     * Test of toByte method, of class org.apache.jcs.yajcache.config.FileContentType.
+     */
+    public void test() {
+        log.debug("test toByte");
+        Byte bJavaSerialization = CacheFileContentType.JAVA_SERIALIZATION.toByte();
+        Byte bXmlEncoder = CacheFileContentType.XML_ENCODER.toByte();
+        assertFalse(bJavaSerialization == bXmlEncoder);
+        log.debug("test fromByte");
+        assertTrue(CacheFileContentType.JAVA_SERIALIZATION == CacheFileContentType.fromByte(bJavaSerialization));
+        assertTrue(CacheFileContentType.XML_ENCODER == CacheFileContentType.fromByte(bXmlEncoder));
+        log.debug("test fromByte with unknown type");
+        try {
+            CacheFileContentType.fromByte((byte)99);
+            fail("Should never get here for unknown fileContentType");
+        } catch(IllegalArgumentException ex) {
+        }
+    }
+}
