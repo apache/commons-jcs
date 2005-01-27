44af7df3517d60db55e54bfc44823f95b7192a2e
Hanson Char <hchar@apache.org>
2005-01-27 10:53:09 +0000

    renamed from CacheChangeListener
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jcs/trunk@224236 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sandbox/yajcache/src/org/apache/jcs/yajcache/core/ICacheChangeListener.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.jcs.yajcache.core;
+
+import org.apache.jcs.yajcache.event.CacheChangeEvent;
+import org.apache.jcs.yajcache.lang.annotation.*;
+
+/**
+ * Cache change event listener/handler.
+ *
+ * @author Hanson CHar
+ */
+@CopyRightApache
+public interface ICacheChangeListener<V> extends java.util.EventListener {
+    public void cacheChange(@NonNullable CacheChangeEvent<V> evt);
+}
