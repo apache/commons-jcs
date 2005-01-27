784b9729dbd52d88cf295c05d65ac071cb263d7a
Hanson Char <hchar@apache.org>
2005-01-27 10:50:17 +0000

    weak reference with a key
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jcs/trunk@224230 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sandbox/yajcache/src/org/apache/jcs/yajcache/lang/ref/KeyedWeakReference.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.jcs.yajcache.lang.ref;
+
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+
+import org.apache.jcs.yajcache.lang.annotation.*;
+/**
+ * @author Hanson Char
+ */
+public class KeyedWeakReference<T> extends WeakReference<T> implements IKey {
+    public final String key;
+
+    public KeyedWeakReference(String key, T referrent, 
+            ReferenceQueue<? super T> q) 
+    {
+        super(referrent);
+        this.key = key;
+    }
+    @Implements(IKey.class)
+    public String getKey() {
+        return this.key;
+    }
+}
