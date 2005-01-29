c6b855af86b16b1d2b288b67211f74896d6a26d5
Hanson Char <hchar@apache.org>
2005-01-29 13:30:57 +0000

    delay lock instantiation + add counts + annotate
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jcs/trunk@224280 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/yajcache/src/org/apache/jcs/yajcache/util/concurrent/locks/KeyedReadWriteLock.java
+++ sandbox/yajcache/src/org/apache/jcs/yajcache/util/concurrent/locks/KeyedReadWriteLock.java
@@ -19,11 +19,15 @@ package org.apache.jcs.yajcache.util.concurrent.locks;
 import java.lang.ref.ReferenceQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.jcs.yajcache.lang.ref.KeyedRefCollector;
 import org.apache.jcs.yajcache.lang.ref.KeyedWeakReference;
+
+import org.apache.jcs.yajcache.lang.annotation.*;
 
 
 /**
@@ -32,14 +36,27 @@ import org.apache.jcs.yajcache.lang.ref.KeyedWeakReference;
  *
  * @author Hanson Char
  */
+@CopyRightApache
 public class KeyedReadWriteLock<K> implements IKeyedReadWriteLock<K> {
-    private final ConcurrentMap<K, KeyedWeakReference<K,ReadWriteLock>> rwlMap = 
+    private static final boolean debug = true;
+    
+    private final @NonNullable ConcurrentMap<K, KeyedWeakReference<K,ReadWriteLock>> rwlMap = 
             new ConcurrentHashMap<K, KeyedWeakReference<K,ReadWriteLock>>();
-    private final Class<? extends ReadWriteLock> rwlClass;
-    private final ReferenceQueue<ReadWriteLock> refQ = 
+    private final @NonNullable Class<? extends ReadWriteLock> rwlClass;
+    private final @NonNullable ReferenceQueue<ReadWriteLock> refQ = 
             new ReferenceQueue<ReadWriteLock>();
-    private final KeyedRefCollector<K> collector = 
+    private final @NonNullable KeyedRefCollector<K> collector = 
             new KeyedRefCollector<K>(refQ, rwlMap);
+    
+    private final AtomicInteger countRWLockCreate = new AtomicInteger(0);
+    private final AtomicInteger countReadLock = new AtomicInteger(0);
+    private final AtomicInteger countWriteLock = new AtomicInteger(0);
+
+    private final AtomicInteger countKeyHit = new AtomicInteger(0);
+    private final AtomicInteger countLockRefEmpty = new AtomicInteger(0);
+    private final AtomicInteger countLockNew = new AtomicInteger(0);
+    private final AtomicInteger countLockExist = new AtomicInteger(0);
+    
     public KeyedReadWriteLock() {
         this.rwlClass = ReentrantReadWriteLock.class;
     }
@@ -47,15 +64,42 @@ public class KeyedReadWriteLock<K> implements IKeyedReadWriteLock<K> {
         this.rwlClass = rwlClass;
     }
     public Lock readLock(K key) {
+        if (debug)
+            this.countReadLock.incrementAndGet();
         return this.readWriteLock(key).readLock();
     }
     public Lock writeLock(K key) {
+        if (debug)
+            this.countWriteLock.incrementAndGet();
         return this.readWriteLock(key).writeLock();
     }
     private ReadWriteLock readWriteLock(K key) {
+        this.collector.run();
+        KeyedWeakReference<K,ReadWriteLock> prevRef = this.rwlMap.get(key);
+        
+        if (prevRef != null) {
+            // existing lock may exist
+            if (debug)
+                this.countKeyHit.incrementAndGet();
+            ReadWriteLock prev = prevRef.get();
+            
+            if (prev != null) {
+                // existing lock
+                if (debug)
+                    this.countLockExist.incrementAndGet();
+                return prev;
+            }
+            if (debug)
+                this.countLockRefEmpty.incrementAndGet();
+            // stale reference; doesn't matter if fail
+            this.rwlMap.remove(key,  prevRef);
+        }
+        // Instantiate a new RW lock.
         ReadWriteLock newLock = null;
         try {
             newLock = rwlClass.newInstance();
+            if (debug)
+                this.countRWLockCreate.incrementAndGet();
         } catch(IllegalAccessException ex) {
             throw new RuntimeException(ex);
         } catch(InstantiationException ex) {
@@ -65,28 +109,17 @@ public class KeyedReadWriteLock<K> implements IKeyedReadWriteLock<K> {
     }
     private ReadWriteLock getLock(final K key, ReadWriteLock newLock) 
     {
-        this.collector.run();
-        ReadWriteLock prev = null;
-        KeyedWeakReference<K,ReadWriteLock> prevRef = this.rwlMap.get(key);
-        
-        if (prevRef != null) {
-            // existing lock may exist
-            prev = prevRef.get();
-            
-            if (prev != null) {
-                // existing lock
-                return prev;
-            }
-            // stale reference; doesn't matter if fail
-            this.rwlMap.remove(key,  prevRef);
-        }
         final KeyedWeakReference<K,ReadWriteLock> newLockRef = 
                 new KeyedWeakReference<K,ReadWriteLock>(key, newLock, refQ);
+        ReadWriteLock prev = null;
+        KeyedWeakReference<K,ReadWriteLock> prevRef = null;
         do {
             prevRef = this.rwlMap.putIfAbsent(key, newLockRef);
 
             if (prevRef == null) {
                 // succesfully deposited the new lock.
+                if (debug)
+                    this.countLockNew.incrementAndGet();
                 return newLock;
             }
             // existing lock may exist
@@ -94,13 +127,26 @@ public class KeyedReadWriteLock<K> implements IKeyedReadWriteLock<K> {
             
             if (prev != null) {
                 // exist lock
+                if (debug)
+                    this.countLockExist.incrementAndGet();
                 return prev;
             }
             // stale reference; doesn't matter if fail
+            if (debug)
+                this.countLockRefEmpty.incrementAndGet();
             this.rwlMap.remove(key,  prevRef);
         } while(true);
     }
-//    public void removeUnusedLocks() {
-//        this.collector.run();
-//    }
+    @Override public String toString() {
+        return new ToStringBuilder(this)
+            .append("\n").append("countReadLock", this.countReadLock)
+            .append("\n").append("countWriteLock", this.countWriteLock)
+            .append("\n").append("countRWLockCreate", this.countRWLockCreate)
+            .append("\n").append("countKeyHit", this.countKeyHit)
+            .append("\n").append("countLockExist", this.countLockExist)
+            .append("\n").append("countLockNew", this.countLockNew)
+            .append("\n").append("countLockRefEmpty", this.countLockRefEmpty)
+            .append("\n").append("collector", this.collector)
+            .toString();
+    }
 }
