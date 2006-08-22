ce8debb5237434d3043b313aef4d2e460cdc0d79
Aaron Smuts <asmuts@apache.org>
2006-08-22 15:19:11 +0000

    Added license header to several new files. Updated servlet dependency to 2.3. Added a utility servlet context listener that can be used to shutdown JCS.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jcs/trunk@433672 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/jcs/utils/servlet/JCSServletContextListener.java
@@ -0,0 +1,71 @@
+package org.apache.jcs.utils.servlet;
+
+/*
+ * Copyright 2001-2004 The Apache Software Foundation. Licensed under the Apache
+ * License, Version 2.0 (the "License") you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.jcs.engine.control.CompositeCacheManager;
+
+/**
+ * If you add this to the context listeners section of your web.xml file, this will shutdown JCS
+ * gracefully.
+ * <p>
+ * Add the following to the top of your web.xml file.
+ * 
+ * <pre>
+ *  &lt;listener&gt;
+ *  &lt;listener-class&gt;
+ *  org.apache.jcs.utils.servlet.JCSServletContextListener
+ *  &lt;/listener-class&gt;
+ *  &lt;/listener&gt;
+ * </pre>
+ * 
+ * @author Aaron Smuts
+ */
+public class JCSServletContextListener
+    implements ServletContextListener
+{
+    private static final Log log = LogFactory.getLog( JCSServletContextListener.class );
+
+    /**
+     * This does nothing. We don't want to initialize the cache here.
+     * <p>
+     * (non-Javadoc)
+     * @see javax.servlet.ServletContextListener#contextInitialized(javax.servlet.ServletContextEvent)
+     */
+    public void contextInitialized( ServletContextEvent arg0 )
+    {
+        if ( log.isInfoEnabled() )
+        {
+            log.info( "contextInitialized" );
+        }
+    }
+
+    /**
+     * This gets the singleton instance of the CompositeCacheManager and calls shutdown.
+     * <p>
+     * (non-Javadoc)
+     * @see javax.servlet.ServletContextListener#contextDestroyed(javax.servlet.ServletContextEvent)
+     */
+    public void contextDestroyed( ServletContextEvent arg0 )
+    {
+        if ( log.isInfoEnabled() )
+        {
+            log.info( "contextDestroyed, shutting down JCS." );
+        }
+        CompositeCacheManager.getInstance().shutDown();
+    }
+
+}
