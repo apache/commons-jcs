a410922e532920d18e083f7210a8f0fdc7cd24bc
Aaron Smuts <asmuts@apache.org>
2009-06-11 14:36:01 +0000

    Adding tests for the double linked list
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/jcs/trunk@783799 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/jcs/utils/struct/DoubleLinkedListUnitTest.java
+++ src/test/org/apache/jcs/utils/struct/DoubleLinkedListUnitTest.java
@@ -1,11 +1,51 @@
 package org.apache.jcs.utils.struct;
 
+import java.io.StringWriter;
+
 import junit.framework.TestCase;
+
+import org.apache.jcs.TestLogConfigurationUtil;
 
 /** Unit tests for the double linked list. */
 public class DoubleLinkedListUnitTest
     extends TestCase
 {
+    /** verify that the last is added when the list is empty. */
+    public void testAddLast_Empty()
+    {
+        // SETUP
+        DoubleLinkedList list = new DoubleLinkedList();
+        
+        String payload1 = "payload1";
+        DoubleLinkedListNode node1 = new DoubleLinkedListNode( payload1 );       
+        
+        // WO WORK
+        list.addLast( node1 );
+               
+        // VERIFY
+        assertEquals( "Wrong last", node1, list.getLast() );
+    }
+    
+    /** verify that the last is added when the list is empty. */
+    public void testAddLast_NotEmpty()
+    {
+        // SETUP
+        DoubleLinkedList list = new DoubleLinkedList();
+        
+        String payload1 = "payload1";
+        DoubleLinkedListNode node1 = new DoubleLinkedListNode( payload1 );       
+        
+        String payload2 = "payload2";
+        DoubleLinkedListNode node2 = new DoubleLinkedListNode( payload2 );
+        
+        // WO WORK
+        list.addLast( node1 );
+        list.addLast( node2 );
+               
+        // VERIFY
+        assertEquals( "Wrong last", node2, list.getLast() );
+    }
+    
     /** verify that it's added last. */
     public void testMakeLast_wasFirst()
     {
@@ -100,5 +140,32 @@ public class DoubleLinkedListUnitTest
         assertEquals( "Wrong size", 3, list.size() );
         assertEquals( "Wrong last", node1, list.getLast() );
         assertEquals( "Wrong first", node3, list.getFirst() );
+    }
+    
+    /** verify that the entries are dumped. */
+    public void testDumpEntries_DebugTrue()
+    {
+        // SETUP
+        StringWriter stringWriter = new StringWriter();
+        TestLogConfigurationUtil.configureLogger( stringWriter, DoubleLinkedList.class.getName() );
+        
+        DoubleLinkedList list = new DoubleLinkedList();
+        
+        String payload1 = "payload1";
+        DoubleLinkedListNode node1 = new DoubleLinkedListNode( payload1 );       
+        
+        String payload2 = "payload2";
+        DoubleLinkedListNode node2 = new DoubleLinkedListNode( payload2 );
+        
+        list.addLast( node1 );
+        list.addLast( node2 );
+        list.debugDumpEntries();
+        
+        // WO WORK
+        String result = stringWriter.toString();
+               
+        // VERIFY
+        assertTrue( "Missing node in log dump", result.indexOf( payload1 ) != -1 );
+        assertTrue( "Missing node in log dump", result.indexOf( payload2 ) != -1 );
     }
 }
