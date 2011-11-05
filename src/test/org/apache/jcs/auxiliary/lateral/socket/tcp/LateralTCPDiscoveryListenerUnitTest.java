6720d521f061da00b378dae2c9fda8cc232a197a
Thomas Vandahl <tv@apache.org>
2011-11-05 19:18:07 +0000

    Forward exception
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/jcs/trunk@1198022 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/jcs/auxiliary/lateral/socket/tcp/LateralTCPDiscoveryListenerUnitTest.java
+++ src/test/org/apache/jcs/auxiliary/lateral/socket/tcp/LateralTCPDiscoveryListenerUnitTest.java
@@ -34,7 +34,7 @@ public class LateralTCPDiscoveryListenerUnitTest
     protected IElementSerializer elementSerializer;
 
     /** Create the listener for testing */
-    protected void setUp()
+    protected void setUp() throws Exception
     {
         cacheMgr = CompositeCacheManager.getInstance();
         cacheEventLogger = new MockCacheEventLogger();
