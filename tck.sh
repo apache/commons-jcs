959d27ce542e0cba8373468f99dbedaecd26534d
Olivier Lamy <olamy@apache.org>
2014-04-30 01:10:12 +0000

    start working on rat fixing
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/jcs/trunk@1591165 13f79535-47bb-0310-9956-ffa450edef68

--- tck.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# build [jcs] first
-mvn clean install -Dmaven.test.skip=true
-
-# then run tck against freshly built binaries
-mvn clean -f tck.xml test surefire-report:report-only -Dmaven.test.failure.ignore=true $@
-
