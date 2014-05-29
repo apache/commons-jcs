42cff055c3d497a67fbf5bebdce2a75c2e31f090
Romain Manni-Bucau <rmannibucau@apache.org>
2014-05-29 16:23:27 +0000

    fixing jcache bash script
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/jcs/trunk@1598326 13f79535-47bb-0310-9956-ffa450edef68

--- jcache-fast.sh
+++ jcache-fast.sh
@@ -2,7 +2,7 @@
 
 cd commons-jcs-core && mvn clean install -Dmaven.test.skip=true && cd -
 cd commons-jcs-jcache && mvn clean install && cd -
-cd commons-jcs-openjpa && mvn clean install && cd -
-cd commons-jcs-extras && mvn clean install && cd -
+cd commons-jcs-jcache-openjpa && mvn clean install && cd -
+cd commons-jcs-jcache-extras && mvn clean install && cd -
 cd commons-jcs-tck-tests && mvn clean install -Djcache.tck && cd -
 
