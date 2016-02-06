70a9d5f1e7ea844758ce3ed9b4f5e46a7015431c
Thomas Vandahl <tv@apache.org>
2016-02-06 18:38:31 +0000

    Add verification of block disk cache key file.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/jcs/trunk@1728866 13f79535-47bb-0310-9956-ffa450edef68

--- commons-jcs-core/src/test/java/org/apache/commons/jcs/auxiliary/disk/block/BlockDiskCacheKeyStoreUnitTest.java
+++ commons-jcs-core/src/test/java/org/apache/commons/jcs/auxiliary/disk/block/BlockDiskCacheKeyStoreUnitTest.java
@@ -19,156 +19,168 @@ package org.apache.commons.jcs.auxiliary.disk.block;
  * under the License.
  */
 
-import org.apache.commons.jcs.auxiliary.disk.behavior.IDiskCacheAttributes.DiskLimitType;
-
 import junit.framework.TestCase;
+
+import org.apache.commons.jcs.auxiliary.disk.behavior.IDiskCacheAttributes.DiskLimitType;
 
 /**
  * Tests for the keyStore.
  * <p>
+ *
  * @author Aaron Smuts
  */
 public class BlockDiskCacheKeyStoreUnitTest
-    extends TestCase
+        extends TestCase
 {
     /** Directory name */
     private final String rootDirName = "target/test-sandbox/block";
 
     /**
-     * Put a bunch of keys inthe key store and verify that they are present.
+     * Put a bunch of keys in the key store and verify that they are present.
      * <p>
+     *
      * @throws Exception
      */
     public void testPutKeys()
-        throws Exception
+            throws Exception
     {
         // SETUP
         BlockDiskCacheAttributes attributes = new BlockDiskCacheAttributes();
-        attributes.setCacheName( "testPutKeys" );
-        attributes.setDiskPath( rootDirName );
-        attributes.setMaxKeySize( 1000 );
-        attributes.setBlockSizeBytes( 2000 );
+        attributes.setCacheName("testPutKeys");
+        attributes.setDiskPath(rootDirName);
+        attributes.setMaxKeySize(1000);
+        attributes.setBlockSizeBytes(2000);
 
         innerTestPutKeys(attributes);
-            }
+    }
 
     public void testPutKeysSize()
             throws Exception
-            {
+    {
         // SETUP
         BlockDiskCacheAttributes attributes = new BlockDiskCacheAttributes();
-        attributes.setCacheName( "testPutKeys" );
-        attributes.setDiskPath( rootDirName );
-        attributes.setMaxKeySize( 100000 );
-        attributes.setBlockSizeBytes( 1024 );
+        attributes.setCacheName("testPutKeys");
+        attributes.setDiskPath(rootDirName);
+        attributes.setMaxKeySize(100000);
+        attributes.setBlockSizeBytes(1024);
         attributes.setDiskLimitType(DiskLimitType.SIZE);
 
         innerTestPutKeys(attributes);
-            }
+    }
 
-
-    private void innerTestPutKeys(BlockDiskCacheAttributes attributes) {
-        BlockDiskCache<String, String> blockDiskCache = new BlockDiskCache<String, String>( attributes );
-
-        BlockDiskKeyStore<String> keyStore = new BlockDiskKeyStore<String>( attributes, blockDiskCache );
+    private void innerTestPutKeys(BlockDiskCacheAttributes attributes)
+    {
+        BlockDiskCache<String, String> blockDiskCache = new BlockDiskCache<String, String>(attributes);
+        BlockDiskKeyStore<String> keyStore = new BlockDiskKeyStore<String>(attributes, blockDiskCache);
 
         // DO WORK
         int numElements = 100;
-        for ( int i = 0; i < numElements; i++ )
+        for (int i = 0; i < numElements; i++)
         {
-            keyStore.put( String.valueOf( i ), new int[i] );
+            keyStore.put(String.valueOf(i), new int[i]);
         }
-//        System.out.println( "testPutKeys " + keyStore );
+        // System.out.println( "testPutKeys " + keyStore );
 
         // VERIFY
-        assertEquals( "Wrong number of keys", numElements, keyStore.size() );
-        for ( int i = 0; i < numElements; i++ )
+        assertEquals("Wrong number of keys", numElements, keyStore.size());
+        for (int i = 0; i < numElements; i++)
         {
-            int[] result = keyStore.get( String.valueOf( i ) );
-            assertEquals( "Wrong array returned.", i, result.length );
+            int[] result = keyStore.get(String.valueOf(i));
+            assertEquals("Wrong array returned.", i, result.length);
         }
     }
 
     /**
-     * Verify that we can load keys that we saved. Add a bunch. Save them. Clear the memory keyhash.
-     * Load the keys. Verify.
+     * Verify that we can load keys that we saved. Add a bunch. Save them. Clear
+     * the memory key hash. Load the keys. Verify.
      * <p>
+     *
      * @throws Exception
      */
     public void testSaveLoadKeys()
-        throws Exception
+            throws Exception
     {
         // SETUP
         BlockDiskCacheAttributes attributes = new BlockDiskCacheAttributes();
-        attributes.setCacheName( "testSaveLoadKeys" );
-        attributes.setDiskPath( rootDirName );
-        attributes.setMaxKeySize( 10000 );
-        attributes.setBlockSizeBytes( 2000 );
+        attributes.setCacheName("testSaveLoadKeys");
+        attributes.setDiskPath(rootDirName);
+        attributes.setMaxKeySize(10000);
+        attributes.setBlockSizeBytes(2000);
 
         testSaveLoadKeysInner(attributes);
-            }
+    }
 
     public void testSaveLoadKeysSize()
             throws Exception
-            {
+    {
         // SETUP
         BlockDiskCacheAttributes attributes = new BlockDiskCacheAttributes();
-        attributes.setCacheName( "testSaveLoadKeys" );
-        attributes.setDiskPath( rootDirName );
-        attributes.setMaxKeySize( 10000 );
-        attributes.setBlockSizeBytes( 2000 );
+        attributes.setCacheName("testSaveLoadKeys");
+        attributes.setDiskPath(rootDirName);
+        attributes.setMaxKeySize(10000);
+        attributes.setBlockSizeBytes(2000);
 
         testSaveLoadKeysInner(attributes);
-            }
+    }
 
-
-
-    private void testSaveLoadKeysInner(BlockDiskCacheAttributes attributes) {
-        BlockDiskKeyStore<String> keyStore = new BlockDiskKeyStore<String>( attributes, null );
+    private void testSaveLoadKeysInner(BlockDiskCacheAttributes attributes)
+    {
+        BlockDiskKeyStore<String> keyStore = new BlockDiskKeyStore<String>(attributes, null);
 
         // DO WORK
         int numElements = 1000;
-        //Random random = new Random( 89 );
-        for ( int i = 0; i < numElements; i++ )
+        int blockIndex = 0;
+        // Random random = new Random( 89 );
+        for (int i = 0; i < numElements; i++)
         {
-            int blocks = i;//random.nextInt( 10 );
-            keyStore.put( String.valueOf( i ), new int[blocks] );
-            keyStore.put( String.valueOf( i ), new int[i] );
+            int blocks = i;// random.nextInt( 10 );
+
+            // fill with reasonable data to make verify() happy
+            int[] block1 = new int[blocks];
+            int[] block2 = new int[blocks];
+            for (int j = 0; j < blocks; j++)
+            {
+                block1[j] = blockIndex++;
+                block2[j] = blockIndex++;
+            }
+            keyStore.put(String.valueOf(i), block1);
+            keyStore.put(String.valueOf(i), block2);
         }
-//        System.out.println( "testSaveLoadKeys " + keyStore );
+        // System.out.println( "testSaveLoadKeys " + keyStore );
 
         // VERIFY
-        assertEquals( "Wrong number of keys", numElements, keyStore.size() );
+        assertEquals("Wrong number of keys", numElements, keyStore.size());
 
         // DO WORK
         keyStore.saveKeys();
         keyStore.clearMemoryMap();
 
         // VERIFY
-        assertEquals( "Wrong number of keys after clearing memory", 0, keyStore.size() );
+        assertEquals("Wrong number of keys after clearing memory", 0, keyStore.size());
 
         // DO WORK
         keyStore.loadKeys();
 
         // VERIFY
-        assertEquals( "Wrong number of keys after loading", numElements, keyStore.size() );
-        for ( int i = 0; i < numElements; i++ )
+        assertEquals("Wrong number of keys after loading", numElements, keyStore.size());
+        for (int i = 0; i < numElements; i++)
         {
-            int[] result = keyStore.get( String.valueOf( i ) );
-            assertEquals( "Wrong array returned.", i, result.length );
+            int[] result = keyStore.get(String.valueOf(i));
+            assertEquals("Wrong array returned.", i, result.length);
         }
     }
 
-    public void testObjectLargerThanMaxSize() {
+    public void testObjectLargerThanMaxSize()
+    {
         BlockDiskCacheAttributes attributes = new BlockDiskCacheAttributes();
-        attributes.setCacheName( "testObjectLargerThanMaxSize" );
-        attributes.setDiskPath( rootDirName );
-        attributes.setMaxKeySize( 1000 );
-        attributes.setBlockSizeBytes( 2000 );
+        attributes.setCacheName("testObjectLargerThanMaxSize");
+        attributes.setDiskPath(rootDirName);
+        attributes.setMaxKeySize(1000);
+        attributes.setBlockSizeBytes(2000);
         attributes.setDiskLimitType(DiskLimitType.SIZE);
 
         @SuppressWarnings({ "unchecked", "rawtypes" })
-        BlockDiskKeyStore<String> keyStore = new BlockDiskKeyStore<String>( attributes, new BlockDiskCache(attributes));
+        BlockDiskKeyStore<String> keyStore = new BlockDiskKeyStore<String>(attributes, new BlockDiskCache(attributes));
 
         keyStore.put("1", new int[1000]);
         keyStore.put("2", new int[1000]);
